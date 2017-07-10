992534fb2614dcce48f4f0af3e1ea1f2a24aecb1
omgs <omgs01@gmail.com>
2017-07-10 15:12:30 +0200

    Add entity class

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class UserType extends AbstractType
 {
