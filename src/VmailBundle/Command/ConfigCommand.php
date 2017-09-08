29403498a764be421d6dc1a2105a066e187baa21
omgs <omgs01@gmail.com>
2017-09-08 04:01:08 +0200

    Add config command

---
+++ src/VmailBundle/Command/ConfigCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace VmailBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use VmailBundle\Utils\DeliverMail;
+use Doctrine\ORM\EntityManager;
+
+class ConfigCommand extends ContainerAwareCommand
+{
+    protected $body;
+
+    protected function configure()
+    {
+        $this
+            ->setName('vmail:config')
+            ->setDescription('Manage database config parameters')
+            ->addOption('all', 'a', InputOption::VALUE_NONE, 'Just print all records')
+            ->addArgument('name', InputArgument::OPTIONAL, 'Config name')
+            ->addArgument('value', InputArgument::OPTIONAL, 'Set value for name')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $all = $input->getOption('all');
+        $name = $input->getArgument('name');
+        $value = $input->getArgument('value');
+        $c=$this->getContainer();
+        $em = $c->get('doctrine.orm.entity_manager');
+
+        $output->writeln("Arguments: all: $all, name: ${name}, value: ${value}");
+        if ($all) {
+            $configs=$em->getRepository('VmailBundle:Config')->findAll();
+            foreach ($configs as $key => $config) {
+                $output->writeln("name: ".$config->getName().", value: ".$config->getValue().", description: ". $config->getDescription());
+            }
+        } else {
+            $config=$em->getRepository('VmailBundle:Config')->findOneByName($name);
+
+            if (!$config) {
+                return $output->writeln("Parameter $name not found. Exiting");
+            }
+
+            if (!empty($value)) {
+                $config->setValue($value);
+                $em->persist($config);
+                $em->flush();
+            }
+            $output->writeln("$name: ".$config->getValue());
+        }
+
+    }
+
+}
