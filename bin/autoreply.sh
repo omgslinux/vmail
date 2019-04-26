ec25792e6c89dc1427eeaa52a84d57013d96d05f
omgs <omgs01@gmail.com>
2019-04-26 14:48:27 +0200

    Crear bucle para procesar varios destinatarios

--- bin/autoreply.sh
+++ bin/autoreply.sh
@@ -5,11 +5,25 @@ cd $(dirname $0)
 cd ..
 
 input=$(cat)
-bodyfile=~/tmp/vmail-$$.tmp
+bodyfile=~/tmp/vmail$$.tmp
 echo "$input" > $bodyfile
 bodysize=$(cat $bodyfile|wc -c)
 sender=$1
-mailbox=$2
-domain=${3##autoreply.}
-logger -t autoreply "Sender: $sender Recipient: $mailbox@$domain, bodyfile: $bodyfile ($bodysize bytes)"
-echo $bodyfile|bin/console vmail:autoreply $sender $mailbox@$domain
+
+## Apaño mientras no se soluciona la query
+PARAMS="$@"
+NUMPARAMS="$#"
+recipients=$(( NUMPARAMS / 2 ))
+logger -t autoreply "Todo: $NUMPARAMS: ($PARAMS) Sender: $sender Destinatarios: $recipients, bodyfile: $bodyfile ($bodysize bytes)"
+while [[ $(echo $1|grep -v 'autoreply.') ]]
+do
+	mailbox=$1
+	domain=${!dparam##autoreply.}
+    logger -t autoreply "Recipient: $mailbox@$domain($dparam)"
+	#echo "bin/console vmail:autoreply $sender $mailbox@$domain"
+    echo $bodyfile|bin/console vmail:autoreply $sender $mailbox@$domain
+   	shift
+done
+## Fin de apaño mientras no se soluciona la query
+
+#echo $bodyfile|bin/console vmail:autoreply $sender $mailbox@$domain
