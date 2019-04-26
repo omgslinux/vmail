f70aeed572c9ac0f61771d76be286326f23311c1
omgs <omgs01@gmail.com>
2019-04-26 15:32:44 +0200

    Poner contador en los destinatarios

--- bin/autoreply.sh
+++ bin/autoreply.sh
@@ -15,12 +15,14 @@ PARAMS="$@"
 NUMPARAMS="$#"
 recipients=$(( NUMPARAMS / 2 ))
 dparam=$(( recipients + 1 ))
+contador=0
 logger -t autoreply "Todo: $NUMPARAMS: ($PARAMS) Sender: $sender Destinatarios: $recipients, bodyfile: $bodyfile ($bodysize bytes)"
 while [[ $(echo $1|grep -v 'autoreply.') ]]
 do
+    contador=$(( contador + 1 ))
     mailbox=$1
     domain=${!dparam##autoreply.}
-    logger -t autoreply "Recipient: $mailbox@$domain($dparam)"
+    logger -t autoreply "($contador de $recipients) Recipient: $mailbox@$domain($dparam)"
     #echo "bin/console vmail:autoreply $sender $mailbox@$domain"
     echo $bodyfile|bin/console vmail:autoreply $sender $mailbox@$domain
     shift
