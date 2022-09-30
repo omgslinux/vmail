f4b3e50df1c820c1480eca5d7181e594f39d8071
omgslinux <omgs01@gmail.com>
2022-09-30 16:20:11 +0200

    Ultimos cambios para form de usuarios

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -5,10 +5,12 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Domain;
 use App\Utils\UserForm;
 use App\Form\UserType;
+use App\Repository\UserRepository as UR;
 
 /**
  * User controller.
@@ -100,7 +102,6 @@ class UserController extends AbstractController
             $this->addFlash('error', 'Correo incorrecto');
             return $this->redirectToRoute(self::PREFIX . 'index');
         }
-        $deleteForm = $this->createDeleteForm($entity);
 
         $form = $this->createForm(UserType::class, $entity, ['showDomain' => false]);
         $form->handleRequest($request);
@@ -115,7 +116,6 @@ class UserController extends AbstractController
         return $this->render('user/show.html.twig', array(
             'entity' => $entity,
             'form' => $form->createView(),
-            'delete_form' => $deleteForm->createView(),
             'PREFIX' => self::PREFIX,
         ));
     }
@@ -127,11 +127,9 @@ class UserController extends AbstractController
      */
     public function showAction(User $user)
     {
-        $deleteForm = $this->createDeleteForm($user);
 
         return $this->render('user/show.html.twig', array(
             'user' => $user,
-            'delete_form' => $deleteForm->createView(),
             'PREFIX' => self::PREFIX,
         ));
     }
@@ -143,7 +141,6 @@ class UserController extends AbstractController
      */
     public function editAction(Request $request, UserForm $u, User $entity, $parent = false)
     {
-        $deleteForm = $this->createDeleteForm($entity);
         $user_form = $this->createForm(
             UserType::class,
             $entity,
@@ -169,43 +166,19 @@ class UserController extends AbstractController
         return $this->render('user/_form.html.twig', array(
             'entity' => $entity,
             'form' => $user_form->createView(),
-            'delete_form' => $deleteForm->createView(),
             'PREFIX' => self::PREFIX,
         ));
     }
 
     /**
-     * Deletes a user entity.
-     *
-     * @Route("/{id}", name="delete", methods={"DELETE"})
+     * @Route("/{id}", name="delete", methods={"POST"})
      */
-    public function deleteAction(Request $request, User $user)
+    public function delete(Request $request, User $entity, UR $repo): Response
     {
-        $form = $this->createDeleteForm($user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($user);
-            $em->flush($user);
+        if ($this->isCsrfTokenValid('delete'.$entity->getId(), $request->request->get('_token'))) {
+            $repo->remove($entity, true);
         }
 
-        return $this->redirectToRoute(self::PREFIX . 'index');
-    }
-
-    /**
-     * Creates a form to delete a user entity.
-     *
-     * @param User $user The user entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(User $user)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl(self::PREFIX . 'delete', array('id' => $user->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
+        return $this->redirectToRoute(self::PREFIX . 'index', [], Response::HTTP_SEE_OTHER);
     }
 }
