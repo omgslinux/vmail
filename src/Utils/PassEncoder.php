4c51e2fdb1045190fd6f4b8d40bbb30ba96e89c8
omgslinux <omgs01@gmail.com>
2022-10-17 01:41:40 +0200

    Eliminar PSR

--- src/Utils/PassEncoder.php
+++ src/Utils/PassEncoder.php
@@ -9,33 +9,34 @@ class PassEncoder implements PasswordHasherInterface
 {
     const SPECIALCHARS = '~!@#$%^&*_-+=`|\(){}[]:;"\'<>,.?/';
 
-  	/**
-  	 * Parameters used for crypt: const name, salt prefix, len of random salt, postfix
-  	 *
-  	 * @var array
-  	 */
-  	private $crypt_params = array(	//
-  		'crypt' => array('', 2, ''),
-  		'ext_crypt' => array('_J9..', 4, ''),
-  		'md5_crypt' => array('$1$', 8, '$'),
-  		'blowfish_crypt' => array('$2a$12$', 22, ''),	// $2a$12$ = 2^12 = 4096 rounds
-  		'sha256_crypt' => array('$5$', 16, '$'),	// no "round=N$" --> default of 5000 rounds
-  		'sha512_crypt' => array('$6$', 16, '$'),	// no "round=N$" --> default of 5000 rounds
-  	);
+    /**
+     * Parameters used for crypt: const name, salt prefix, len of random salt, postfix
+     *
+     * @var array
+     */
+    private $crypt_params =
+    [
+        'crypt' => array('', 2, ''),
+        'ext_crypt' => array('_J9..', 4, ''),
+        'md5_crypt' => array('$1$', 8, '$'),
+        'blowfish_crypt' => array('$2a$12$', 22, ''), // $2a$12$ = 2^12 = 4096 rounds
+        'sha256_crypt' => array('$5$', 16, '$'), // no "round=N$" --> default of 5000 rounds
+        'sha512_crypt' => array('$6$', 16, '$'), // no "round=N$" --> default of 5000 rounds
+    ];
 
-  	private	$random_char =
-      [
-  			'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f',
-  			'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
-  			'w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L',
-  			'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
-  		]
+    private $random_char =
+    [
+        '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f',
+        'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+        'w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L',
+        'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+    ]
     ;
 
     private $cryptType;
     private $saltPrefix;
 
-    public function __construct($type='sha512_crypt')
+    public function __construct($type = 'sha512_crypt')
     {
         $this->setType($type);
     }
@@ -46,19 +47,19 @@ class PassEncoder implements PasswordHasherInterface
         //$this->setSalt($type);
     }
 
-    public function encodePassword($raw, $saltPrefix=null)
+    public function encodePassword($raw, $saltPrefix = null)
     {
         // Implementamos nuestro encoder personalizado
-        if (null==$saltPrefix) $saltPrefix=$this->getSaltPrefix($this->cryptType).md5(md5($raw));
+        if (null==$saltPrefix) {
+            $saltPrefix=$this->getSaltPrefix($this->cryptType).md5(md5($raw));
+        }
         //die(dump($raw, $this->cryptType, "salt: ".$this->getSaltPrefix($this->cryptType),$saltPrefix));
-		return crypt($raw, $saltPrefix);
+        return crypt($raw, $saltPrefix);
         //return hash('sha1', $salt . $raw);
     }
 
-    public function isPasswordValid($encoded, $raw, $salt=null)
+    public function isPasswordValid($encoded, $raw, $salt = null)
     {
-				//list($const, $prefix, $len, $postfix) = self::$crypt_params[$type];
-        //if (is_null($salt)) $salt=$this->getSalt();
         return $encoded === $this->encodePassword($raw, $salt);
     }
 
@@ -81,60 +82,61 @@ class PassEncoder implements PasswordHasherInterface
     private function checkType($type)
     {
         //$salt='';
-				list($prefix, $chars, $suffix) = $this->crypt_params[$type];
+        list($prefix, $chars, $suffix) = $this->crypt_params[$type];
         // return $this->randomstring($chars).$suffix;
         //$salt=$prefix;
         switch (strtolower($type)) {
-          case 'sha512_crypt':
-			      if (!(defined('CRYPT_SHA512') && constant('CRYPT_SHA512') == 1)) {
-                return $this->checkType('sha256_crypt');
-            }
-            break;
-          case 'sha256_crypt':
-            if (!(defined('CRYPT_SHA256') && constant('CRYPT_SHA256') == 1)) {
-                return $this->checkType('md5_crypt');
-            }
-            break;
-          case 'md5_crypt':
-            if (!(defined('CRYPT_MD5') && constant('CRYPT_MD5') == 1)) {
-                return $this->checkType('ext_crypt');
-            }
-            break;
-          case 'ext_crypt':
-            if (!(defined('CRYPT_EXT_DES') && constant('CRYPT_EXT_DES') == 1)) {
-                return $this->checkType('crypt');
-            }
-            break;
-          case 'crypt':
-            if (!(defined('CRYPT_STD_DES') && constant('CRYPT_STD_DES') == 1)) {
-                return false;
-            }
-            break;
-
-          }
+            case 'sha512_crypt':
+                if (!(defined('CRYPT_SHA512') && constant('CRYPT_SHA512') == 1)) {
+                    return $this->checkType('sha256_crypt');
+                }
+                break;
+            case 'sha256_crypt':
+                if (!(defined('CRYPT_SHA256') && constant('CRYPT_SHA256') == 1)) {
+                    return $this->checkType('md5_crypt');
+                }
+                break;
+            case 'md5_crypt':
+                if (!(defined('CRYPT_MD5') && constant('CRYPT_MD5') == 1)) {
+                    return $this->checkType('ext_crypt');
+                }
+                break;
+            case 'ext_crypt':
+                if (!(defined('CRYPT_EXT_DES') && constant('CRYPT_EXT_DES') == 1)) {
+                    return $this->checkType('crypt');
+                }
+                break;
+            case 'crypt':
+                if (!(defined('CRYPT_STD_DES') && constant('CRYPT_STD_DES') == 1)) {
+                    return false;
+                }
+                break;
+        }
         $this->setSaltPrefix($prefix);
         $this->type=$type;
 
         return $type;
     }
 
-    public function getRandomString($size, $use_specialchars=false)
+    public function getRandomString($size, $use_specialchars = false)
     {
       // we need special chars
-      if ($use_specialchars)
-      {
-        $random_char = array_merge($this->random_char, str_split(str_replace('\\', '', self::SPECIALCHARS)), $random_char);
-      }
+        if ($use_specialchars) {
+            $random_char = array_merge(
+                $this->random_char,
+                str_split(str_replace('\\', '', self::SPECIALCHARS)),
+                $random_char
+            );
+        }
 
-      // use cryptographically secure random_int available in PHP 7+
-      $func = function_exists('random_int') ? 'random_int' : 'mt_rand';
+        // use cryptographically secure random_int available in PHP 7+
+        $func = function_exists('random_int') ? 'random_int' : 'mt_rand';
 
-      $s = '';
-      for ($i=0; $i < $size; $i++)
-      {
-        $s .= $random_char[$func(0, count($random_char)-1)];
-      }
-      return $s;
+        $s = '';
+        for ($i = 0; $i < $size; $i++) {
+            $s .= $random_char[$func(0, count($random_char)-1)];
+        }
+        return $s;
     }
 
     public function hash(string $plainPassword): string
@@ -153,5 +155,4 @@ class PassEncoder implements PasswordHasherInterface
     {
         return false;
     }
-
 }
