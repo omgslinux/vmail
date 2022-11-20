44b5491b966fd7a0ff7dd9fb1b4f212da6c26b65
Oscar <omgs01@gmail.com>
2022-11-20 19:13:36 +0100

    Eliminar error de no encontrar ruta

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -220,11 +220,11 @@ dump($alias);
             $session->remove('useredit');
 
             $ur->formSubmit($user_form);
-
+dump($origin);
             if (null==$origin) {
                 return $this->redirectToRoute(self::VARS['PREFIX'] . 'index');
             } else {
-                return $this->redirectToRoute($origin);
+                return $this->redirectToRoute('admin_domain_showbyname', [ 'name' => $origin ]);
                 //return $this->redirectToRoute('admin_domain_show', ['id' => $entity->getDomain()->getId()]);
                 //return $this->redirectToRoute(self::PREFIX . 'edit', array('id' => $entity->getId()));
             }
