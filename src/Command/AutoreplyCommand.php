b8b05af92c17e9096cc3c31c70b697b25907bffc
omgslinux <omgs01@gmail.com>
2024-01-19 23:41:03 +0100

    Remove mistakes

--- src/Command/AutoreplyCommand.php
+++ src/Command/AutoreplyCommand.php
@@ -2,13 +2,13 @@
 
 namespace App\Command;
 
-#use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use App\Utils\AutoreplyMail;
+use Symfony\Component\Console\Attribute\AsCommand;
 
 #[AsCommand(
     name: 'vmail:autoreply',
